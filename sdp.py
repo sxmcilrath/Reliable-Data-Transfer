121457e0126d19b2c7e36a144f44bb3964fbc50d
Devin J. Pohly <djpohly@gmail.com>
2020-02-08 15:25:38 -0600

    change SDP semantics to broadcast to sockets
    Demonstrates interaction between Socket class and Protocol class a bit
    more clearly.

--- sdp.py
+++ sdp.py
@@ -2,13 +2,27 @@ from network import Protocol, DatagramSocket
 
 
 class SampleDatagramSocket(DatagramSocket):
-    """Socket class for SampleDatagramProtocol"""
+    """Socket class for SampleDatagramProtocol
+
+    Subclasses of Socket can access the associated Protocol instance via the
+    self.proto variable.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new socket.
+
+        This constructor is called when creating a socket with socket().
+        """
+
+        super().__init__(*args, **kwargs)
+        # Note: RDT may want to register sockets at a different point...
+        self.proto.add_to_list(self)
 
     def input(self, data, src):
         self.deliver(data, src)
 
     def sendto(self, msg, dst):
-        # SDP has no header; segment = message
+        # SDP has no header, so message == segment
         self.output(msg, dst)
 
 
@@ -22,15 +36,23 @@ class SampleDatagramProtocol(Protocol):
     arrives for this protocol.
     """
 
-    PROTO_ID = 99
+    PROTO_ID = 0xfd
     SOCKET_CLS = SampleDatagramSocket
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.sock = super().socket()
+        self.socks = []
 
-    def socket(self):
-        return self.sock
+    # Helper method, specific to SDP
+    def add_to_list(self, sock):
+        self.socks.append(sock)
 
     def input(self, seg, src):
-        self.sock.input(seg, src)
+        """Called by the network layer when a packet arrives.
+
+        Instead of multiplexing, SDP broadcasts every incoming segment to all
+        SDP sockets.  Don't follow this example - RDT will pass segments only
+        to the socket they belong to.
+        """
+        for s in self.socks:
+            s.input(seg, src)
--- test_sdp.py
+++ test_sdp.py
@@ -16,8 +16,9 @@ class A_SDPTest(unittest.TestCase):
         self.h2 = Host(self.n, '192.168.10.2')
         self.h1.register_protocol(SampleDatagramProtocol)
         self.h2.register_protocol(SampleDatagramProtocol)
-        self.s1 = self.h1.socket(99)
-        self.s2 = self.h2.socket(99)
+        self.s1 = self.h1.socket(SampleDatagramProtocol.PROTO_ID)
+        self.s2 = self.h2.socket(SampleDatagramProtocol.PROTO_ID)
+        self.s2a = self.h2.socket(SampleDatagramProtocol.PROTO_ID)
 
     def test_oneway(self):
         self.s1.sendto(b'hello', '192.168.10.2')
@@ -27,6 +28,36 @@ class A_SDPTest(unittest.TestCase):
         self.assertEqual(self.s2.recvfrom(), (b'hello', '192.168.10.1'))
         self.assertEqual(self.s2.recvfrom(), (b'', '192.168.10.1'))
         self.assertEqual(self.s2.recvfrom(), (b' world', '192.168.10.1'))
+
+    def test_twoway(self):
+        self.s1.sendto(b'hello', '192.168.10.2')
+        self.s1.sendto(b'', '192.168.10.2')
+        self.s1.sendto(b' world', '192.168.10.2')
+        self.s2.sendto(b'hi', '192.168.10.1')
+        self.s2.sendto(b'', '192.168.10.1')
+        self.s2.sendto(b' planet', '192.168.10.1')
+
+        self.assertEqual(self.s2.recvfrom(), (b'hello', '192.168.10.1'))
+        self.assertEqual(self.s2.recvfrom(), (b'', '192.168.10.1'))
+        self.assertEqual(self.s2.recvfrom(), (b' world', '192.168.10.1'))
+        self.assertEqual(self.s1.recvfrom(), (b'hi', '192.168.10.2'))
+        self.assertEqual(self.s1.recvfrom(), (b'', '192.168.10.2'))
+        self.assertEqual(self.s1.recvfrom(), (b' planet', '192.168.10.2'))
+
+    def test_broadcast(self):
+        self.s1.sendto(b'hello', '192.168.10.2')
+        self.s1.sendto(b'', '192.168.10.2')
+        self.s1.sendto(b' world', '192.168.10.2')
+
+        self.assertEqual(self.s2.recvfrom(), (b'hello', '192.168.10.1'))
+        self.assertEqual(self.s2.recvfrom(), (b'', '192.168.10.1'))
+
+        self.assertEqual(self.s2a.recvfrom(), (b'hello', '192.168.10.1'))
+
+        self.assertEqual(self.s2.recvfrom(), (b' world', '192.168.10.1'))
+
+        self.assertEqual(self.s2a.recvfrom(), (b'', '192.168.10.1'))
+        self.assertEqual(self.s2a.recvfrom(), (b' world', '192.168.10.1'))
 
 if __name__ == '__main__':
     unittest.main()
