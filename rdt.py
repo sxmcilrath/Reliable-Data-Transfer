ee2330bd98a6e6af7984e41797de8d6719115965
Samuel McIlrath <samuelmcilrath@cslab19.wheaton.edu>
2025-10-13 00:00:34 -0500

    socket bind and listen wip

--- rdt.py
+++ rdt.py
@@ -1,4 +1,8 @@
 from network import Protocol, StreamSocket
+import threading
+import sys
+import os
+import random
 
 # Reserved protocol number for experiments; see RFC 3692
 IPPROTO_RDT = 0xfe
@@ -8,12 +12,36 @@ class RDTSocket(StreamSocket):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Other initialization here
+        self.is_connected = False
+        self.is_bound = False
+        self.port = None
+        self.lock = threading.Lock()
 
     def bind(self, port):
-        pass
+
+        self.lock.acquire()
+        #if in use by another system
+        if(self.proto.bound_ports[port]):
+            raise StreamSocket.AddressInUse
+        #check if alr connected
+        if(self.is_connected):
+            raise StreamSocket.AlreadyConnected
+        
+        #set fields and bind
+        self.port = port
+        self.is_bound = True
+        StreamSocket.bind(port)
+
+        self.lock.release()
+
+        
 
     def listen(self):
-        pass
+        self.lock.acquire()
+
+        #err checking
+        if(not self.is_bound):
+            raise StreamSocket.NotBound
 
     def accept(self):
         pass
@@ -32,6 +60,8 @@ class RDTProtocol(Protocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Other initialization here
+        self.bound_ports = {} #bool dictionary if ports are being used
+        self.lock = threading.Lock()
 
     def input(self, seg, rhost):
         pass
